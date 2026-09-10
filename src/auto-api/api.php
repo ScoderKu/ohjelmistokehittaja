2277f782a731dc2316ddcc5eef2b99d3b0e3f12d
Khoi Sk <phan.nguyen@edu.sakky.fi>
2026-09-10 19:44:12 +0300

    add db

---
+++ src/auto-api/api.php
@@ -0,0 +1,13 @@
+<?php
+$pdo = new PDO(
+    "mysql:host=db;dbname=autokanta;charset=utf8mb4",
+    "user",
+    "pass"
+);
+
+$sql = "SELECT * FROM autot";
+$stmt = $pdo->query($sql);
+$autot = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header("Content-Type: application/json");
+echo json_encode($autot);
